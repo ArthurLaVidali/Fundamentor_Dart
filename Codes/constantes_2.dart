ac8d235e2a3cdab7ed7bca370aaaeccba483e1da
ArthurLVidal <arthurodri2017@gmail.com>
2024-02-05 10:42:18 -0300

    Add constantes_2.dart

---
+++ Codes/constantes_2.dart
@@ -0,0 +1,11 @@
+main() {
+  final lista = [ // Se você colocar const aqui, não será possivel adicionar valores a lista
+    'Ana',
+    'Arthur',
+    'Anthony',
+    'Caua'
+  ]; // Não é possvivel atribuir outro valor a lista, mas é possivel adicionar valores a ela
+  lista.add('Ihago'); // Adiciona a lista
+
+  print(lista);
+}
