e4cf9642de347d432345cb8351f43fd542a9c489
ArthurLVidal <arthurodri2017@gmail.com>
2024-02-22 13:13:34 -0300

    Add desafioifElse.dart

---
+++ Controle/desafioifElse.dart
@@ -0,0 +1,12 @@
+main() {
+  var nota = 2.3;
+
+  if (nota >= 9.0) {
+    // Se você colocar um ; no final do if, ele irá executar o bloco de código
+    print('Parabéns! Você foi brilhante!'); // Não será exibido esta frase no console
+  }
+
+  if (nota <= 0.5);{
+    print('Este bloco será printado');
+  }
+}
--- Controle/ifelse.dart
+++ Controle/ifelse.dart
@@ -5,7 +5,8 @@ main() {
       Random().nextInt(11); // => Vai gerar um valer aleatorio menor que 11
   if (nota > 6) {
     print('Com a sua nota ($nota) você foi: Aprovado!');
-  }if (nota == 10{
+  }
+  if (nota == 10) {
     print('Você tem nota máxima!');
   } else {
     print('Com a sua nota ($nota) você foi: Reprovado');
@@ -13,5 +14,6 @@ main() {
 
   if (false)
     print('Não será apresentado na saída'); // => Como o if é falso, e não tem {} ele não vai apresentar a próxima linha
-    print('Porém este print será apresentado na saída'); // Normalmente coloca-se dentro de algum bloco "{}"
+  print(
+      'Porém este print será apresentado na saída'); // Normalmente coloca-se dentro de algum bloco "{}"
 }
