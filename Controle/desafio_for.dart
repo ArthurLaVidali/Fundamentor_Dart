1e5d3589aa6f465739a754fb0dff3de34af8a4e7
ArthurLVidal <arthurodri2017@gmail.com>
2024-03-03 00:03:07 -0300

    Add desafio_for.dart

---
+++ Controle/desafio_for.dart
@@ -0,0 +1,14 @@
+// Utilizando for imprima
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// Mas não pode controlar o laço usando valor númerico!
+
+main(){
+  for(var a="#"; a!="#######" ; a+="#"){
+    print(a);
+  }
+}
