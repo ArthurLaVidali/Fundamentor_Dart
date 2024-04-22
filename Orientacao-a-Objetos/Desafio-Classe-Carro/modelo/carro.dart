68f64b977a7b042522ef8f3c48a3f81b9b850088
ArthurLVidal <arthurodri2017@gmail.com>
2024-04-22 15:04:02 -0300

    Adicionando get e set para o Desafio Carro

--- Orientacao-a-Objetos/Desafio-Classe-Carro/modelo/carro.dart
+++ Orientacao-a-Objetos/Desafio-Classe-Carro/modelo/carro.dart
@@ -1,28 +1,45 @@
 class Carro {
-  final int velocidadeMaxima;
-  int velocidadeAtual = 0;
+  final int velocidadeMaxima; // ==> final = já que não pode ser alterada
+  int _velocidadeAtual = 0;
+  // ==> Coloca-se uma _ antes de velocidadeAtual para não ser possível ver esse atributo fora do arquivo carro.dart
 
   Carro([this.velocidadeMaxima = 200]) {}
 
   int acelerar() {
-    if (velocidadeAtual + 5 >= velocidadeMaxima) {
-      velocidadeAtual = velocidadeMaxima;
+    if (_velocidadeAtual + 5 >= velocidadeMaxima) {
+      _velocidadeAtual = velocidadeMaxima;
     } else {
-      velocidadeAtual += 5;
+      _velocidadeAtual += 5;
     }
-    return velocidadeAtual;
+    return _velocidadeAtual;
   }
 
   int frear() {
-    if (velocidadeAtual - 5 <= 0) {
-      velocidadeAtual = 0;
+    if (_velocidadeAtual - 5 <= 0) {
+      _velocidadeAtual = 0;
     } else {
-      velocidadeAtual -= 5;
+      _velocidadeAtual -= 5;
     }
-    return velocidadeAtual;
+    return _velocidadeAtual;
   }
 
   bool estaNoLimite() {
-    return velocidadeAtual == velocidadeMaxima;
+    return _velocidadeAtual == velocidadeMaxima;
   }
+
+  // Como _velocidadeAtual não está visivel para fora do arquivo
+  // É necessário adicionar um Get para receber o valor da velocidadeAtual
+  int get velocidadeAtual {
+    return _velocidadeAtual;
+  }
+
+  // ==> Colocando _ antes de velocidadeAtual, e criando apenas um get para receber o valor da variavel
+  // Não é possível alterar a velocidadeAtual fora da classe, a menos que seja criado um Set para isso
+
+  void set velocidadeAtual(int novaVelocidade) {
+    bool deltaValido = (_velocidadeAtual - novaVelocidade).abs() <= 5;
+    if (deltaValido && novaVelocidade >= 0) {
+      this._velocidadeAtual = novaVelocidade;
+    }
+  } // ==> E desta forma criamos um set para velocidade atual
 }
